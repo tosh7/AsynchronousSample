0d31797d99b3585a2b91cc1e00318a2254e16ab9
tosh7 <zlia.6.lj.425@gmail.com>
2018-12-06 23:49:32 +0900

    initial commit

---
+++ AsynchronousSample/Sources/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AsynchronousSample
+//
+//  Created by Satoshi Komatsu on 2018/12/06.
+//  Copyright © 2018 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    var getImage = GetImage()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
---
+++ AsynchronousSample/Sources/Model/GetImage.swift
@@ -0,0 +1,17 @@
+//
+//  GetImage.swift
+//  AsynchronousSample
+//
+//  Created by Satoshi Komatsu on 2018/12/06.
+//  Copyright © 2018 Satoshi Komatsu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class GetImage {
+    var image: UIImage!
+    
+    init(){}
+    
+}
